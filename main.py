efad509da82a47103b60483d3691cb5bc1576da3
Kenneth <kmsullivanjr@uri.edu>
2023-12-13 19:57:21 -0500

    Started using Flask web framework. Refined and completed the restock, purchase, and check and display stock functions. Stock is stored inside of a json file in the root directory. API calls from postman are successfull in GET and POST methods.

--- main.py
+++ main.py
@@ -1,23 +1,99 @@
-from admin import adm_check_stock, adm_restock, check_stock
-from customer import cst_display_stock, cst_purchase
+# To build the python backened and allow for HTTP API calls, we will use the Flask web framework
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+with open('stock.json') as f:
+    current_soda_stock = json.load(f)
+
+print(current_soda_stock)
+
+## ADMIN FUNCTIONS
+
+# Check Stock
+# API Definition to check stock
+@app.route('/api/admin/check_stock', methods=['GET'])
+def adm_check_stock():
+    
+    # This function is called from the Admin HTTP API
+    # Function should return to the admin:
+    # Each soda and its description, current price, quantity, and maxQuantity.
+     
+    with open('stock.json') as f:
+        current_soda_stock = json.load(f)    
+    return jsonify(current_soda_stock)
+
+# API Definition to restock machine
+@app.route('/api/admin/restock', methods=['POST'])
+def adm_restock():
+    
+        
+    
+    # This Function is called from the Admin HTTP API
+    # Function takes from the API:
+    # The Soda name and amount of stock to add to the machine
+    # Its important here to check the stock of the machine already
+    # to ensure that there is enough room for the desired restock quanitity.
+    
+    with open('stock.json') as f:
+        current_soda_stock = json.load(f)
+    
+    incoming = request.json
+    sodaName = incoming["sodaName"]
+    restockQuantity = incoming["restockQuantity"]
+    maxRestock = current_soda_stock[sodaName]["maxQuantity"]
+    available = current_soda_stock[sodaName]["available"]
+    
+    if available + restockQuantity <= maxRestock:
+        current_soda_stock[sodaName]["available"] += restockQuantity
+    
+    with open('stock.json', 'w') as f:
+        json.dump(current_soda_stock, f)
+    
+    return jsonify(current_soda_stock)
+
+## Customer Functions
+
+@app.route('/api/display_stock', methods=['GET'])
+def cst_display_stock():
+    with open('stock.json') as f:
+        current_soda_stock = json.load(f)    
+    return jsonify(current_soda_stock)
+
+
+
+
+@app.route('/api/customer_purchase', methods=['POST'])
+def cst_purchase():    
+    with open('stock.json') as f:
+        current_soda_stock = json.load(f)
+    
+    incoming = request.json
+    sodaName = incoming["sodaName"]
+    if current_soda_stock[sodaName]["available"] != 0:
+        current_soda_stock[sodaName]["available"] -= 1
+    with open('stock.json', 'w') as f:
+        json.dump(current_soda_stock, f)
+    
+    return jsonify(current_soda_stock)
+    
 
 
 
 ## Need some dictionary / Key-Value data structure to represent Virtual Sodas.
 ### Ideally, design is fluent within HTML design as well
-POP_info = {'description': 'An explosion of flavor that will knock your socks off!', 'price': 1, "maxQuantity": 100}
-FIZZ_info = {'description':'An effervescent fruity experience with hints of grape and coriander','price': 1, "maxQuantity": 100}
-COLA_info = {'description':'A basic no nonsense cola that is the perfect pick me up for any occasion.', 'price': 1, 'maxQuantity': 200}
-MEGAPOP_info = {'description':'Not for the faint of heart. So flavorful and so invigorating, it shold probably be illegal.'}
 
-SODAS = [FIZZ_info, POP_info, COLA_info, MEGAPOP_info] 
+
 MACHINE_CASH = 100
 
-# Variable to control the active state of the vending machine
-run = True
-# Main Loop
-while run:
-    
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
+        
+
     # Function to display current stock { Soda/Desc/Price/Quant }
     
     
@@ -44,5 +120,3 @@ while run:
             # If Checkstock:
                 # adm_checkstock
         
-    # If user hits Exit
-        # run = False
